dd629e94bad00646521b2c88164ab0ea80ff8134
MansuraAkhter <mansura3330@gmail.com>
2021-02-21 20:13:53 +0300

    add comment box styling

--- frontend/components/CommentListItem.js
+++ frontend/components/CommentListItem.js
@@ -5,8 +5,8 @@ function CommentListItem(props) {
     return(
         <div className = "cl">
             <div className = "cl-comment" >{props.comment.comment}</div>
-            <button className = "cl-vote" onClick={() =>{props.like(props.comment.commentID)}}>{props.comment.upVoteSum} <i className="far fa-thumbs-up "></i></button>
-            <button className = "cl-vote" onClick={() =>{props.dislike(props.comment.commentID)}}>{props.comment.downVoteSum} <i className="far fa-thumbs-down"></i></button>
+            <button className = "cl-vote-up" onClick={() =>{props.like(props.comment.commentID)}}>{props.comment.upVoteSum} <i className="far fa-thumbs-up "></i></button>
+            <button className = "cl-vote-down" onClick={() =>{props.dislike(props.comment.commentID)}}>{props.comment.downVoteSum} <i className="far fa-thumbs-down"></i></button>
            
 
 
